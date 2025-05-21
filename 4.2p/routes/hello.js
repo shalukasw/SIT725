3611ba58af746df7f6d89472e444cc091f6f927a
shalukasw <70917991+shalukasw@users.noreply.github.com>
2025-05-21 14:24:18 +1000

    Delete 4.2p directory

--- 4.2p/routes/hello.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Simple GET route
-router.get('/', (req, res) => {
-  res.send('Hello from the /api/hello route!');
-});
-
-module.exports = router;
